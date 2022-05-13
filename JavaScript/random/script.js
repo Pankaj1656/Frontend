80faa6f9110bcd04d90888376d060716f1a73aab
Pankaj1656 <pc69269@gmail.com>
2022-05-13 23:12:16 +0530

    changes

---
+++ JavaScript/random/script.js
@@ -0,0 +1,17 @@
+let p=new Promise((resolve,reject)=>{
+    let a= 1+1;
+    if(a==2){
+        resolve('Success')
+    }
+    else{
+        reject('Failed')
+    }
+})
+
+p.then((message)=>{
+    console.log('This is in then '+ message)
+})
+.catch((message)=>{
+    console.log('This is in Catch '+ message)
+
+})
